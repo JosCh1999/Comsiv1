2223189a44d860478ca21aa0e9a54aee968069ae
JosCh1999 <75557750@continental.edu.pe>
2025-05-11 21:27:39 -0500

    first commit

---
+++ Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+const BASE_URL = "http://localhost/tienda-comsitec/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "tienda-comsitec";
+const CHARSET = "charset=utf8";
+const TITLE = "TIENDA ONLINE";
+const MONEDA = "USD";
+const CLIENT_ID = "AQNh2Y8HxyKSjTtLdyOaYlmRhnBjnJqkOvZFc8DkWj-1EU4Kth52poytb-538WdvdWuq9i6jSyGPedWP";
+
+const USER_SMTP = "korlant69@gmail.com";
+const PASS_SMTP = "uwqgnzizbankcpsx";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
